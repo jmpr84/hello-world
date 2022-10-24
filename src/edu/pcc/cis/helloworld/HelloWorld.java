1a7c1c42fad5d0697a76773136d87c828cbd4339
Jose Peralta <jose.peralta@pcc.edu>
2022-10-23 21:09:46 -0700

    adding additinal sentence to show date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -4,7 +4,9 @@ import java.util.Date;
 public class HelloWorld {
     public static void main (String[] arg)
     {
+        System.out.println("##################");
         System.out.println("Hello World");
-        System.out.println(new Date());
+        System.out.println("Today's date is: " + new Date());
+        System.out.println("##################");
     }
 }
